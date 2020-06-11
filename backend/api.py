80dfa1bf21bdba769416f2a8a8b4d04971fc50b6
Felipe <vilella.luisfelipe@gmail.com>
2020-06-11 19:38:28 -0300

    testing create charge

--- backend/api.py
+++ backend/api.py
@@ -50,5 +50,20 @@ def filter_entity(type_entity: schemas.EntityTypeEnum = None,
 
 
 @app.post(_VERSION + "/charge", response_model=schemas.Charge)
-def create_charge(charge: schemas.ChargeCreate):
-    return schemas.Charge(**charge.dict())
+def create_charge(charge: schemas.ChargeCreate,
+                  db: sqlalchemy.orm.Session = fastapi.Depends(get_db)):
+    db_charge = data_access.get_charge_by_creditor_cpf_cnpj(db, creditor_cpf_cnpj=charge.creditor_cpf_cnpj)
+    if db_charge:
+        raise fastapi.HTTPException(status_code=400, detail="Charge alredy exist")
+
+    return data_access.create_charge(db=db, charge=charge)
+
+
+
+@app.get(_VERSION + "/charge/{creditor_cpf_cnpj}", response_model=schemas.Charge)
+def read_charge(creditor_cpf_cnpj: str, db: sqlalchemy.orm.Session = fastapi.Depends(get_db)):
+    db_charge = data_access.get_charge_by_creditor_cpf_cnpj(db, creditor_cpf_cnpj=creditor_cpf_cnpj)
+    if not db_charge:
+        raise fastapi.HTTPException(status_code=404, detail="Charge does not exist")
+
+    return db_charge
--- backend/data_access.py
+++ backend/data_access.py
@@ -4,15 +4,10 @@ import uuid
 from . import models, schemas
 
 
+# ENTITY THINGS
+
 def get_entity_by_cpf_cnpj(db: sqlalchemy.orm.Session, cpf_cnpj: str) -> models.Entity:
     return db.query(models.Entity).get(cpf_cnpj)
-
-def get_charge_by_id(db: sqlalchemy.orm.Session, id_uuid: str) -> models.Charge:
-    query = db.query(models.Charge).get(id_uuid)
-    if not query:
-        return None
-    
-    return query
 
 
 def create_entity(db: sqlalchemy.orm.Session, entity: schemas.EntityCreate, persist:bool = True) -> models.Entity:
@@ -34,6 +29,16 @@ def filter_entity_by_type(db: sqlalchemy.orm.Session,
     return query.all()
 
 
+# CHARGE THINGS
+
+def get_charge_by_id(db: sqlalchemy.orm.Session, id_uuid: str) -> models.Charge:
+    return db.query(models.Charge).get(id_uuid)
+
+
+def get_charge_by_creditor_cpf_cnpj(db: sqlalchemy.orm.Session, creditor_cpf_cnpj: str) -> models.Charge:
+    return db.query(models.Charge).get(creditor_cpf_cnpj)
+
+
 def create_charge(db: sqlalchemy.orm.Session, charge: schemas.ChargeCreate) -> models.Charge:
     debtor_db = get_entity_by_cpf_cnpj(db, charge.debtor.cpf_cnpj)
     creditor_db = get_entity_by_cpf_cnpj(db, charge.creditor_cpf_cnpj)
@@ -41,12 +46,11 @@ def create_charge(db: sqlalchemy.orm.Session, charge: schemas.ChargeCreate) -> m
     if not debtor_db:
         debtor_db = create_entity(db, charge.debtor, persist=False)
 
-    id_uuid = uuid.uuid4
-    if get_charge_by_id(db, id_uuid):
-        raise ValueError("Charge alredy exist")
+    # id_uuid = uuid.uuid4()
+    # if get_charge_by_id(db, id_uuid):
+    #     raise ValueError("Charge alredy exist")
     
-    db_charge = models.Charge(id = id_uuid,
-                              debtor_cpf_cnpj = debtor_db.cpf_cnpj,
+    db_charge = models.Charge(debtor_cpf_cnpj = debtor_db.cpf_cnpj,
                               creditor_cpf_cnpj = creditor_db.cpf_cnpj,
                               debito = charge.debito,
                               is_active = True)
